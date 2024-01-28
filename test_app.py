abd4744c6c763901075de433e704cc7802f5c825
Gerold Busch <gerold.busch@gmail.com>
2024-01-28 20:51:41 +0100

    test should fail on purpose

--- test_app.py
+++ test_app.py
@@ -12,6 +12,17 @@ def test_france_quiz():
     ), f"expected answer to include {expected_subjects}, but didn't"
 
 
+def test_france_quiz_failonpurpose():
+    app = llm_chain()
+    question = "Tell me 3 facts about France."
+    answer = app.invoke({"question": question})
+    expected_subjects = ["xxx"]
+    print(answer)
+    assert any(
+        subject.lower() in answer.lower() for subject in expected_subjects
+    ), f"expected answer to include {expected_subjects}, but didn't"
+
+
 def test_italy_quiz():
     app = llm_chain()
     question = "Tell me something about Italy."
